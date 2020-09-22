16bb6ae8d2ef1e29164179064d68ef2deed5fcbc
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-09-22 13:15:10 +0300

    wip-project

--- crypto-app/src/App.js
+++ crypto-app/src/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
-
-import './App.css';
+import React, { useState, useEffect } from "react";
+//axios
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      )
+      .then((response) => {
+        setCoins(response.data);
+        // console.log(res.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div className="crypto-app">
+      <div className="crypto-search">
+        <h1 className="crypto-text">Search a crypto currency</h1>
+        <form>
+          <input type="text" placeholder="Search" className="crypto-input"/>
+        </form>
+      </div>
+      
     </div>
+
   );
 }
 
