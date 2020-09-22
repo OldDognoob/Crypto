e6719e96b9fbeb64f5fc75f2c42b828e189c9c66
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-09-22 13:23:08 +0300

    crypto-file-wip

---
+++ crypto-app/src/Crypto.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Crypto = () => {
+    return (
+        <div className="crypto-container">
+            <div className="crypto-row">
+                <div className="crypto">
+                    <img src={image}alt="crypto"/>
+                    <h1>{name}</h1>
+                    <p classNAME="crypto-symbol">{symbol}</p>
+                </div>
+                <div className="crypto-data">
+                    <p className="crypto-price">Eur{price}</p>
+                    <p className="crypto-volume">Eur{volume.toLocaleString()}</p>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Crypto
